f85764e32d0fe1bd92fd9c2de5924981ab684830
Ankit Patel <yugap1806@gmail.com>
2025-02-21 21:19:45 +0530

    Ankit Patel Added HelloAPI UC1

---
+++ src/main/java/com/springboot/messaging_app/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.springboot.messaging_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping
+    public String sayHello(){return "Hello from BridgeLabz";}
+}
